6a9ec37d9aa1ccc4236ba60a652f01166f7020db
ezenardr <ezenardr.dev@gmail.com>
2023-09-02 09:15:33 -0400

    fixed sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: 'https://quimonteabord.com.com',
+            url: 'https://quimonteabord.com',
             lastModified: new Date(),
         },
     ];
